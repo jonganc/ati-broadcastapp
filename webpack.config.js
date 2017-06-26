4dfa60b0e64fc84c019d3b07280f33cb2032d231
Jonathan Ganc <jonganc@gmail.com>
2017-06-25 22:45:34 -0400

    fixed travis failure

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,54 @@
 const webpack = require('webpack');
 const path = require('path');
 
-// we need babel to load config
-
-module.exports = async () => {
-  // basic configuration
-  const webpackConfig = {
-    entry: {
-      app: ['./src/App.jsx'],
-      vendor: ['react', 'react-dom', 'whatwg-fetch', 'react-router', 'moment'],
-    },
-    output: {
-      path: path.resolve(__dirname, 'static'),
-      filename: 'app.bundle.js',
-    },
-    plugins: [
-      new webpack.optimize.CommonsChunkPlugin(
-        { name: 'vendor', filename: 'vendor.bundle.js' }),
-    ],
-    module: {
-      loaders: [
-        {
-          test: /\.jsx$/,
-          loader: 'babel-loader',
-          query: {
-            presets: [
-              ['env', {
-                targets: ['> 4%'] },
-              ],
-              'react',
+// basic configuration
+const webpackConfig = {
+  entry: {
+    app: ['./src/App.jsx'],
+    vendor: ['react', 'react-dom', 'whatwg-fetch', 'react-router', 'moment'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'static'),
+    filename: 'app.bundle.js',
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin(
+      { name: 'vendor', filename: 'vendor.bundle.js' }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            ['env', {
+              targets: ['> 4%'] },
             ],
-          },
+            'react',
+          ],
         },
-      ],
-    },
-    devtool: 'source-map',
-  };
+      },
+    ],
+  },
+  devtool: 'source-map',
+};
 
-  //  If we are on the dev server, add appropriate section.
-  //  We separate this out because we only want to load babel-register if
-  //  we have to
-  const isDevServer
-        = process.argv.find(v => v.includes('webpack-dev-server'));
-  if (isDevServer) {
-    // eslint-disable-next-line global-require
-    require('babel-register')({ only: /server\/config/ });
-    // eslint-disable-next-line global-require
-    const configReq = require('./server/config');
-    const config = await (configReq.default());
+let toExport;
+const isDevServer
+      = process.argv.find(v => v.includes('webpack-dev-server'));
+if (!isDevServer) {
+  toExport = Promise.resolve(webpackConfig);
+} else {
+  //  We need babel to load config.js.
+  //  There are complications introduced if we try to avoid this by
+  //  compiling config.js before running the dev server.
 
+  // eslint-disable-next-line global-require
+  require('babel-register')({ only: /server\/config/ });
+  // eslint-disable-next-line global-require
+  const configReq = require('./server/config');
+  toExport = configReq.default().then((config) => {
     webpackConfig.devServer = {
       port: 8000,
       contentBase: 'static',
@@ -59,7 +59,8 @@ module.exports = async () => {
       },
       historyApiFallback: true,
     };
-  }
+    return webpackConfig;
+  });
+}
 
-  return webpackConfig;
-};
+module.exports = toExport;
